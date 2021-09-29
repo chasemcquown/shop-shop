9c84c30bfd8955602447db677975f5c1a0bcd07e
chasem <cpmcquown@gmail.com>
2021-09-29 14:35:30 -0700

    adds persistence to shopping cart items

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,37 +1,40 @@
 import React from 'react';
-
-import { useStoreContext } from '../../utils/GlobalState';
-import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { useStoreContext } from "../../utils/GlobalState";
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
 
-  // NOTE: that we only destructured the dispatch() function from the useStoreContext Hook, because the CartItem component has no need to read state
   const [, dispatch] = useStoreContext();
 
   const removeFromCart = item => {
-      dispatch({
-          type: REMOVE_FROM_CART,
-          _id: item._id
-      });
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: item._id
+    });
+    idbPromise('cart', 'delete', { ...item });
+
   };
 
   const onChange = (e) => {
+    const value = e.target.value;
+    if (value === '0') {
+      dispatch({
+        type: REMOVE_FROM_CART,
+        _id: item._id
+      });
+      idbPromise('cart', 'delete', { ...item });
 
-      const value = e.target.value;
+    } else {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: item._id,
+        purchaseQuantity: parseInt(value)
+      });
+      idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
 
-      if (value === '0') {
-          dispatch({
-              type: REMOVE_FROM_CART,
-              _id: item._id
-          });
-      } else {
-          dispatch({
-              type: UPDATE_CART_QUANTITY,
-              _id: item._id,
-              purchaseQuantity: parseInt(value)
-          });
-      }
-  };
+    }
+  }
 
   return (
     <div className="flex-row">
